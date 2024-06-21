469eb8e492d1a08a874bb2df8265897bf61bc314
Tejas <tejaskarambe.tk@gmail.com>
2024-06-21 20:56:30 +0530

    correct2

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import TodoList from "./TodoList";
 
-
 var destination = document.querySelector('#container');
 
 
 ReactDOM.render(
   <div>
-	<TodoList/>
+	  <TodoList/>
   </div>, 
   destination
 );
